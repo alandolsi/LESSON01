6e0a8865ab3c061652dc27d1c773afeacd66fa6f
Landolsi Webdesign <abdellatif@landolsi.de>
2023-03-29 14:23:03 +0200

    chapter 6

--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,125 @@
 import './style.css';
 
-type One = string;
-type Two = string | number;
-type Three = 'hello';
-
-let a: One = 'hello';
-console.log(a);
-let b = a as Two;
-let c = a as Three;
-
-console.log(c);
-
-let d = <One>'word';
-let e = <string | number>'word';
-
-const addOrConcat = (a: number, b: number, c: 'add' | 'concat'): number | string => {
-  if (c === 'add') {
-    return a + b;
+class Coder {
+  secondLang!: string;
+  constructor(public readonly name: string, public music: string, private age: number, protected lang: string = 'JavaScript') {
+    this.name = name;
+    this.music = music;
+    this.age = age;
+    this.lang = lang;
   }
-  return `${a} ${b}`;
-};
 
-let myValue: string = addOrConcat(1, 2, 'concat') as string;
+  /**
+   * getAge
+   */
+  public getAge() {
+    return `My age is ${this.age}`;
+  }
+}
 
-// Be carefull because typescript sees no problem with this but a string is returned
-let myValue2: number = addOrConcat(1, 2, 'concat') as number;
+const coder = new Coder('John', 'Rock', 25);
 
-// 10 as string;
-10 as unknown as string;
+console.log(coder.getAge());
+console.log('#'.repeat(50));
+class webdev extends Coder {
+  constructor(public computer: string, name: string, music: string, age: number, lang: string) {
+    super(name, music, age, lang);
+    this.computer = computer;
+  }
 
-// the DOM
-const img = document.querySelector('img')!;
-// no null assertion
-const myImage = document.getElementById('myImage')! as HTMLImageElement;
+  public getLang = () => {
+    return `My language is ${this.lang}`;
+  };
+}
 
-const nextImage = <HTMLImageElement>document.getElementById('nextImage');
+const Sara = new webdev('Mac', 'Sara', 'Pop', 22, 'TypeScript');
 
-// img.src;
-// myImage.src;
+console.log(Sara.getLang());
 
-// let year: HTMLElement | null;
-// year = document.getElementById('year');
-// let thisYear: string = new Date().getFullYear().toString();
-// if (year) {
-//   year.setAttribute('datetime', thisYear);
-//   year.textContent = thisYear;
-// }
+console.log('#'.repeat(50));
 
-const year = document.getElementById('year') as HTMLSpanElement;
-const thisYear: string = new Date().getFullYear().toString();
-year.setAttribute('datetime', thisYear);
-year.textContent = thisYear;
+interface Musician {
+  name: string;
+  instrument: string;
+  play: (action: string) => string;
+}
+
+class Guitarist implements Musician {
+  constructor(public name: string, public instrument: string) {
+    this.name = name;
+    this.instrument = instrument;
+  }
+
+  play = (action: string) => {
+    return `${this.name} is ${action} with ${this.instrument}`;
+  };
+}
+
+const John = new Guitarist('John', 'Guitar');
+
+console.log(John.play('playing'));
+
+console.log('#'.repeat(50));
+class Peeps {
+  static count: number = 0;
+
+  static getCount(): number {
+    return Peeps.count;
+  }
+
+  public id: number;
+
+  constructor(public name: string, public age: number) {
+    this.name = name;
+    this.age = age;
+    this.id = ++Peeps.count;
+  }
+}
+
+const Steve = new Peeps('Steve', 30);
+const Sara2 = new Peeps('Sara2', 22);
+const John2 = new Peeps('John2', 25);
+
+console.log(Peeps.count);
+console.log(John2.id);
+console.log(Sara2.id);
+console.log(Steve.id);
+
+console.log('#'.repeat(50));
+
+class Bands {
+  private dataState: string[];
+
+  constructor() {
+    this.dataState = [];
+  }
+
+  public get data(): string[] {
+    return this.dataState;
+  }
+
+  public set data(data: string[]) {
+    if (Array.isArray(data) && data.every((item) => typeof item === 'string')) {
+      this.dataState = data;
+    } else {
+      throw new Error('Invalid data');
+    }
+  }
+}
+
+const bands = new Bands();
+
+bands.data = ['Metallica', 'AC/DC', 'Guns N Roses'];
+
+console.log(bands.data);
+
+bands.data = [...bands.data, 'ZZ Top'];
+
+console.log(bands.data);
+
+bands.data = ['TT Bottom'];
+
+console.log(bands.data);
+
+
+
